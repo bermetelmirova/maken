7875ecc956c47b0aafefa6a6ed03c7b509849e76
bermetelmirova <bermetk.6789@gmail.com>
2021-11-27 13:55:56 +0600

    add project and readme

---
+++ src/main/java/kg/academy/maken/MakenApplication.java
@@ -0,0 +1,13 @@
+package kg.academy.maken;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kg/academy/maken/MakenApplicationTests.java
@@ -0,0 +1,13 @@
+package kg.academy.maken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
